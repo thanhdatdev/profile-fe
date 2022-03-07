33d749ba8febe1b7eef96a367f9ba8be70b689bf
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2022-03-07 19:48:39 +0700

    done intro

--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import avatar from  '../../assets/images/avatar/thanh-dat-nguyen.png'
-import { Typography } from '../../styles/style';
+import { Sizing, Typography } from '../../styles/style';
 
 const Wrapper = styled.div`
   display: flex;
@@ -17,12 +17,17 @@ const Avatar = styled.img`
 `;
 
 const Name = styled.div`
-  line-height: 1.15;
-  ${Typography.bold.x80};
+  ${Typography.bold.x60};
+  @media ${Sizing.device.tablet} {
+    ${Typography.bold.x80};
+  }
 `;
 
 const Text = styled.div`
-  ${Typography.regular.x70}
+  ${Typography.regular.x20}
+  @media ${Sizing.device.tablet} {
+    ${Typography.regular.x70}
+  }
 `;
 
 export default function Intro() {
