2b5a51ce16f24445db7f09d59c3987d1a78cd482
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2022-03-08 14:56:28 +0700

    config dotenv

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Styles from './Styles';
+import useAxios from '../../hooks/useAxios';
 
-export default function Code() {
+
+export default function About() {
+  const { response, loading, error } = useAxios({
+    method: 'post',
+    url: '/api/v1/',
+    headers: JSON.stringify({ accept: '*/*' }),
+    body: JSON.stringify({
+      admin: process.env.USER_NAME,
+      password: process.env.PASS_WORD,
+    }),
+  });
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (response !== null) {
+      setData(response);
+    }
+  }, [response]);
+
+  console.log(data);
   return (
     <>
       <Styles.WrapperContentBlock>
